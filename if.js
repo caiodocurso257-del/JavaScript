c541a0ac93fb26fb2674d8197ca2f8b308992c93
caiodocurso257-del <caiodocurso257@gmail.com>
2026-09-22 15:09:07 -0300

    add if.js

---
+++ if.js
@@ -0,0 +1,7 @@
+let hora = 15;
+
+if (hora < 12) {
+    console.log("Bom dia!")
+}
+// O restante do codigo continua independentemente se a  condição for verdadeira ou não.
+console.log("O dinheiro nao traz feliciade. Me de o seu e seja feliz! 🤣");
