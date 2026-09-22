479fe5eee3fbf88294d41b07c540ae6d00ab72d3
caiodocurso257-del <caiodocurso257@gmail.com>
2026-09-22 15:46:00 -0300

    add if-else-2.js

---
+++ if-else-2.js
@@ -0,0 +1,7 @@
+let diaDaSemana = "quarta-feira";
+
+if(diaDaSemana === "sabado" ||diaDaSemana === "domingo" ) {
+    console.log("É o fim de semana!");
+} else {
+    console.log("É o dia de semana!");
+}
